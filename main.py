fcc2a1a3d95b6c2f926713da4d48b893a5a5071e
amal642 <mohamedamal0345@gmail.com>
2024-03-08 23:35:22 +0530

    delete

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
     allow_credentials=True,
-    allow_methods=["GET", "POST"],
+    allow_methods=["GET", "POST","DELETE"],
     allow_headers=["*"],
 )
 
